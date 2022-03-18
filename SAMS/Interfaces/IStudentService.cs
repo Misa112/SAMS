91df40a682d06ff15b7da91e2670cc5b5e742cc4
Misa112 <michaela.macejovska@yahoo.com>
2022-03-18 12:34:09 +0100

    get all students

---
+++ SAMS/Interfaces/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using SAMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAMS.Interfaces
+{
+    public interface IStudentService
+    {
+        List<Student> GetAllStudents();
+    }
+}
